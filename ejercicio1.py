39d6a5a24b891061c120d9711b38af069ff901bc
patricio <p.carvajal@duocuc.cl>
2024-06-21 12:43:25 -0400

    opcion2_casilista

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,15 +2,10 @@ import os
 import msvcrt
 import csv
 sala_de_cine = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-while True:
-    
-    print(f"""\t>>>Pantalla<<<
-        ================""")
 
-    for x in range(len(sala_de_cine)):
-        
-        print(x+1,f"\t{sala_de_cine[x]}")
-    print("\t================")
+while True:
+
+    
 
     print(">>>Menu<<<")
     print("1) Mostrar asientos")
@@ -21,11 +16,26 @@ while True:
     opc = int(input("seleccione opcion en pantalla: "))
     os.system('cls')
     if opc == 1:
-        pass
+        print(f"""\t>>>Pantalla<<<
+            ================""")
+
+        for x in range(len(sala_de_cine)):
+            print(x+1,f"\t{sala_de_cine[x]}")
+        print("\t================")
+        print(">PRESIONE TECLA PARA CONTINUAR<")
+        msvcrt.getch()
     elif opc == 2:
-        pass
+        nombre = input("ingrese su nombre: ")
+        edad = int(input("ingrese su edad: "))
+        numero_t = int(input("ingrese su numero de telefono: +569"))
+        datos_persona = {"nombre": nombre,
+                           "edad" : edad, 
+                           "numero_t": numero_t}
+        c = int(input("Columna 1 a 5:"))
+        p = int(input("escoja el asiento del 1 al 5: "))
+        sala_de_cine[c][p]= "x"
     elif opc == 3:
-        pass
+        print(datos_persona)
     elif opc == 4:
         pass
     else:
